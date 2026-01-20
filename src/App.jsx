f6855a3c0e73f496e120a700ec15f691486b0c0d
=AshishThanki656 <=>
2026-01-20 12:22:55 +0530

    Changes Made On app and IDcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import IDCard from './Components/IDCard';
+import React, { useState } from "react";
+import IDCard from "./Components/IDCard";
+// import Bulb from './Components/Bulb'
 
 function App() {
+  const [Data, setData] = useState([
+    { id: 1, name: "Rogers", role: "Captain", age: 100 },
+    { id: 2, name: "Stark", role: "Engineer", age: 45 },
+    { id: 3, name: "Banner", role: "Scientist", age: 50 },
+  ]);
+
+  const HandleDelete = (id) => {
+    const newData = Data.filter((d) => d.id !== id);
+    setData(newData);
+  };
+
   return (
     <div>
-
-      <IDCard name="rogers" role = "captain" age={100}/>
-      <IDCard name="stark" role = "engineer" age={45}/>
-      <IDCard name="banner" role = "scientist" age={50}/>
-
+      {Data.map((d) => (
+        <IDCard
+          key={d.id}
+          name={d.name}
+          role={d.role}
+          age={d.age}
+          HandleDelete={() => HandleDelete(d.id)}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/IDCard.jsx
+++ src/Components/IDCard.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 
-function IDCard({ name, role, age }) {
-
+const IDCard = ({ name, role, age, HandleDelete }) => {
   return (
-    <div className="card"
+    <div
+      className="card"  
       style={{
-        backgroundColor: role === "captain" ? "blue" 
-        : role === "engineer"
-        ? "red"  : role === "scientist" ? "green" : "transparent"
+        backgroundColor:
+          role === "captain"
+            ? "blue"
+            : role === "engineer"
+              ? "red"
+              : role === "scientist"
+                ? "green"
+                : "transparent",
       }}
     >
       <h2>{name}</h2>
       <p>Role:{role}</p>
       <p>Age:{age}</p>
+      <button onClick={HandleDelete}>Delete</button>
     </div>
   );
-}
-
+};
 export default IDCard;
